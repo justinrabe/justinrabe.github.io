a504ae5ab1a65e4b7ee1dedb20780e29fed7f2b4
justin <jtr342@gmail.com>
2021-01-10 11:39:53 -0800

    CV website now can run on a server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require ('express');
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send("index.html")
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000, () => {
-    console.log('app listening at http://localhost:3000')
+    console.log('app listening at http://localhost:3000');
 })
